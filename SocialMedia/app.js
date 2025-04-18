e39a419e938001031053581ebedff2c0ee590e32
Apple <apple@MUKUL-AGRAWAL.local>
2025-04-18 12:22:49 +0530

    complete

--- SocialMedia/app.js
+++ SocialMedia/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const app = express();
-// const axios = require('axios');
-// const cors = require('cors');
+app.use(cors());
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.listen(8080,()=>{
     console.log("Server is running on port 8080")
 })
